4198b1d17a6d91b9cddb05f1ec22a1eef99c23e3
shujianbo <shujianbo@itiger.com>
2019-10-30 19:32:44 +0800

    three shaking

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
-import _ from 'lodash';
-import printMe from './print';
-
+import { chunk } from 'lodash';
+import { cube } from './math';
 
 function component() {
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
+    var element = document.createElement('pre');
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
+    element.innerHTML = [
+        'Hello World',
+        '5 cube is equal to ' + cube(5)
+    ].join('\n\n');
 
     return element;
 }
   
 document.body.appendChild(component());
 
-if (module.hot) {
-    module.hot.accept('./print.js', function() {
-        console.log('Accepting the update printMe module');
-        printMe();
-    });
-}
+// if (module.hot) {
+//     module.hot.accept('./print.js', function() {
+//         console.log('Accepting the update printMe module');
+//         printMe();
+//     });
+// }
