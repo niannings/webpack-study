63c3fa6c34cf19687e5295b6a243f44d784034a9
shujianbo <shujianbo@itiger.com>
2019-10-30 17:30:06 +0800

    HMR

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import _ from 'lodash';
-import './style.css';
-import xw from './xw.jpg';
+import printMe from './print';
+
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-    const img = new Image();
-    img.src = xw;
+    element.appendChild(btn);
 
-    element.appendChild(img);
-  
     return element;
 }
   
 document.body.appendChild(component());
+
+if (module.hot) {
+    module.hot.accept('./print.js', function() {
+        console.log('Accepting the update printMe module');
+        printMe();
+    });
+}
