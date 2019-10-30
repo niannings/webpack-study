2e2af98108752a59717feb0b71609c57e8d044c6
shujianbo <shujianbo@itiger.com>
2019-10-30 11:54:29 +0800

    管理资源

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import _ from 'lodash';
+import './style.css';
+import xw from './xw.jpg';
 
 function component() {
     var element = document.createElement('div');
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    const img = new Image();
+    img.src = xw;
+
+    element.appendChild(img);
   
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,56 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+      rules: [
+          {
+              test: /\.css$/,
+              use: [
+                  'style-loader',
+                  'css-loader'
+              ]
+          },
+          {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: 10000 , /* 图片大小小于1000字节限制时会自动转成 base64 码引用*/
+                  name: '[path][name].[ext]?[hash:6]!./dir/file.png'
+                },
+              },
+              {
+                loader: 'image-webpack-loader',
+                options: {
+                  mozjpeg: {
+                    progressive: true,
+                    quality: 65
+                  },
+                  // optipng.enabled: false will disable optipng
+                  optipng: {
+                    enabled: false,
+                  },
+                  pngquant: {
+                    quality: [0.65, 0.90],
+                    speed: 4
+                  },
+                  gifsicle: {
+                    interlaced: false,
+                  },
+                  // the webp option will enable WEBP
+                  webp: {
+                    quality: 75
+                  }
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: ['url-loader']
+        }
+      ]
   }
 };
